079e6d0f3d6ddeb9128dafdf13ad578625996336
Lewis Sneddon <lewis@lewiscodes.io>
2023-02-03 13:17:40 +0000

    sorted footer

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -2,16 +2,16 @@ import './footer.scss';
 
 import React from 'react';
 
-import { Col, Row } from 'reactstrap';
-
 const Footer = () => (
-  <div className="footer page-content">
-    <Row>
-      <Col md="12">
-        <p>This is your footer</p>
-      </Col>
-    </Row>
-  </div>
+  <footer className="w-100 py-4 flex-shrink-0 align-bottom mt-auto">
+    <div className="container py-4">
+      <div className="row gy-4 gx-5">
+        <div className="text-center">
+          <p className="small text-muted">Made for fun by Lewis Sneddon</p>
+        </div>
+      </div>
+    </div>
+  </footer>
 );
 
 export default Footer;
